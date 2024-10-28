13d9ee2c06639b5c74236b05f97bbb313def3811
Vitaly Trakhtenberg <webdev@tca.by>
2024-10-28 22:10:31 +0300

    Class MyArray done, tests passed

--- src/main/java/telran/interview/MyArray.java
+++ src/main/java/telran/interview/MyArray.java
@@ -1,30 +1,39 @@
 package telran.interview;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MyArray<T>
 {
-    //TODO
-    public void setAll(T value) {
-        //TODO
-        //all array's elements should be set with a given value
+    private T[] my_array;
 
-    }
-    public void set(int index, T value) {
-        //TODO
-        //set new value at a given index
-        //throws ArrayIndexOutOfBoundsException for incorrect index
-        throw new UnsupportedOperationException();
-
+    public void setAll(T value)
+    {
+        Arrays.fill(my_array, value);
     }
 
-    public T get(int index) {
-        //returns a value at a given index
-        //throws ArrayIndexOutOfBoundsException for incorrect index
-        throw new UnsupportedOperationException();
+    public void set(int index, T value)
+    {
+        if (index < 0 || index >= my_array.length) {
+            throw new ArrayIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
 
+        my_array[index] = value;
     }
-    public MyArray(int size) {
-        //creates the Array object for a given size
-        //with setting null's at each element
-        throw new UnsupportedOperationException();
+
+    public T get(int index)
+    {
+        if (index < 0 || index >= my_array.length) {
+            throw new ArrayIndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+
+        return my_array[index];
+    }
+    public MyArray(int size)
+    {
+        if (size <= 0) {
+            throw new ArrayStoreException("Size cannot be zero or less");
+        }
+        my_array = (T[]) Array.newInstance(Object.class, size);
     }
 }
