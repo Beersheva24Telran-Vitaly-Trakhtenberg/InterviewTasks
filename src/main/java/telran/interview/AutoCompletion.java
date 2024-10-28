aaf7802dfab5e56f17a01194e8928baa1cf98ecd
Vitaly Trakhtenberg <webdev@tca.by>
2024-10-28 18:58:54 +0300

    Preparing /src from the Yuri's GitHub

---
+++ src/main/java/telran/interview/AutoCompletion.java
@@ -0,0 +1,20 @@
+package telran.interview;
+
+public class AutoCompletion
+{
+    //TODO
+    public boolean addWord(String word) {
+        // TODO Implement this method
+        throw new UnsupportedOperationException("Method AutoCompletion.addWord() not implemented yet");
+        //Note:
+        // adds new word into auto-completion variants
+        // returns true if added, false otherwise (if a given word already exists)
+    }
+    public String [] getVariants(String prefix) {
+        //TODO Implement this method
+        throw new UnsupportedOperationException("Method AutoCompletion.getVariants() not implemented yet");
+        //Note:
+        // returns all words beginning with a given prefix
+        // Complexity of finding the variants is O[logN]
+    }
+}
--- src/main/java/telran/interview/InterviewTasks.java
+++ src/main/java/telran/interview/InterviewTasks.java
@@ -1,16 +1,49 @@
 package telran.interview;
 
-public class InterviewTasks
-{
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+
+public class InterviewTasks {
     /**
-     *
      * @param array
      * @param sum
      * @return true if a given array comprises of two number,
-     *  summing of which gives the value equaled to a given "sum" value
+     * summing of which gives the value equaled to a given "sum" value
      */
-    static public boolean hasSumTwo(int [] array, int sum) {
+    static public boolean hasSumTwo(int[] array, int sum) {
+        HashSet<Integer> helpers = new HashSet<>();
+        int index = 0;
+        while (index < array.length && !helpers.contains(sum - array[index])) {
+            helpers.add(array[index++]);
+        }
+        return index < array.length;
+    }
+
+    static public int getMaxWithNegativePresentation(int[] array) {
         //TODO
-        return false;
+        //returns maximal positive value for which exists negative one with the same abs value
+        //if no pair of positive and negative values with the same abs value the method returns -1
+        //complexity O[N] only one pass over the elements
+        throw new UnsupportedOperationException();
+    }
+
+    public static List<DateRole> assignRoleDates(List<DateRole> rolesHistory,
+                                                 List<LocalDate> dates) {
+        //TODO
+        //rolesHistory is the list containg dates and roles assigned to the employees at the appropriate dates
+        //for example, date => 2019-01-01, role => Developer means that some employee (no matter who) was taken
+        //role Developer at 2019-01-01
+        //create List<DateRole> with roles matching with the given dates
+        //most effective data structure
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean isAnagram(String word, String anagram) {
+        //TODO
+        //returns true if "anagram" string contains all
+        // letters from "word" in another order (case sensitive)
+        //O[N] (sorting is disallowed)
+        throw new UnsupportedOperationException();
     }
 }
