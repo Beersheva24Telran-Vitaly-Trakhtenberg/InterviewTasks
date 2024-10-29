e5353d9d7a3461d5e447851a2ea07bc4b2c91571
Vitaly Trakhtenberg <webdev@tca.by>
2024-10-29 14:14:18 +0300

    refactoring: LinkedHashSet => TreeSet

--- src/main/java/telran/interview/AutoCompletion.java
+++ src/main/java/telran/interview/AutoCompletion.java
@@ -1,26 +1,18 @@
 package telran.interview;
 
-import java.util.*;
+import java.util.TreeSet;
+import java.util.Set;
 
-public class AutoCompletion
-{
-    private final LinkedHashSet<String> words = new LinkedHashSet<>();
+public class AutoCompletion {
+    private Set<String> words = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
 
-    public boolean addWord(String word)
-    {
+    public boolean addWord(String word) {
         return words.add(word);
     }
 
-    public String [] getVariants(String prefix)
-    {
-        List<String> variants = new ArrayList<>();
-        String lower_prefix = prefix.toLowerCase();
-        for (String word : words) {
-            if (word.toLowerCase().startsWith(lower_prefix)) {
-                variants.add(word);
-            }
-        }
-        variants.sort(String.CASE_INSENSITIVE_ORDER);
-        return variants.toArray(new String[0]);
+    public String[] getVariants(String prefix) {
+        return words.stream()
+                .filter(word -> word.toLowerCase().startsWith(prefix.toLowerCase()))
+                .toArray(String[]::new);
     }
 }
